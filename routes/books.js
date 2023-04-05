2553e0663a13f22b9d41521e381ad8b380744322
Thinkedou <r.enguehard@gmail.com>
2023-04-05 15:59:39 +0200

    first route!

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const impDb = require('../services/imp-db');
 
 
-router.get('/', function(req, res) {
-    res.send('Books home page');
-  });
+
+router.get('/',  function(req, res) {
+  const a =  impDb.findMany()
+  res.send(a)
+});
 
 
 router.get('/about', function(req, res) {
     res.send('About Books');
 });
   
-  module.exports = router;
+module.exports = router;
